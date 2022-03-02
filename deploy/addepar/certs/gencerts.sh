57c607f8b9c480867204ba122a31bc69f9b1b02b
Daniel Talamas <daniel.talamas@addepar.com>
2022-03-02 16:56:44 -0600

    Make changes

--- deploy/addepar/certs/gencerts.sh
+++ deploy/addepar/certs/gencerts.sh
@@ -15,14 +15,14 @@ case $(uname) in
             b64_opts='--wrap=0'
 esac
 
-#go get -v -u github.com/cloudflare/cfssl/cmd/...
+go get -v -u github.com/cloudflare/cfssl/cmd/...
 
 export PURPOSE=metrics
 openssl req -x509 -sha256 -new -nodes -days 365 -newkey rsa:2048 -keyout ${PURPOSE}-ca.key -out ${PURPOSE}-ca.crt -subj "/CN=ca"
 echo '{"signing":{"default":{"expiry":"43800h","usages":["signing","key encipherment","'${PURPOSE}'"]}}}' > "${PURPOSE}-ca-config.json"
 
 export SERVICE_NAME=custom-metrics-apiserver
-export ALT_NAMES='"custom-metrics-apiserver.monitoring","custom-metrics-apiserver.monitoring.svc"'
+export ALT_NAMES='"custom-metrics-apiserver.custom-metrics","custom-metrics-apiserver.custom-metrics.svc"'
 echo "{\"CN\":\"${SERVICE_NAME}\", \"hosts\": [${ALT_NAMES}], \"key\": {\"algo\": \"rsa\",\"size\": 2048}}" | \
        	cfssl gencert -ca=metrics-ca.crt -ca-key=metrics-ca.key -config=metrics-ca-config.json - | cfssljson -bare apiserver
 
@@ -32,6 +32,6 @@ kind: Secret
 metadata:
   name: cm-adapter-serving-certs
 data:
-  serving.crt: $(base64 ${b64_opts} < apiserver.pem)
-  serving.key: $(base64 ${b64_opts} < apiserver-key.pem)
+  serving.crt: $(cat apiserver.pem | base64 ${b64_opts})
+  serving.key: $(cat apiserver-key.pem | base64 ${b64_opts})
 EOF
